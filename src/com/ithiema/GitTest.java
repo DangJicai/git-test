bce4b68ad1b2802570c85e2c47aa34f7a0ee34ca
dangjicai <1253611201@qq.com>
2019-07-12 21:05:06 +0800

    新增文件

--- src/com/ithiema/GitTest.java
+++ src/com/ithiema/GitTest.java
@@ -3,5 +3,8 @@ package com.ithiema;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello........");
+
+        System.out.println("新增文件");
+
     }
 }
